0cf7b7437d824f14c415b9900a5f2e3be22254bd
Farhad Zamani <zmnfrhd@gmail.com>
2022-10-31 19:37:32 +0100

    initial commit bounds

--- src/bounds.py
+++ src/bounds.py
@@ -10,13 +10,86 @@ def lower_bound(x: list[int], v: int) -> int:
     """Get the index of the lower bound of v in x.
 
     If all values in x are smaller than v, return len(x).
-    """
-    return 0  # FIXME: Obviously the answer isn't always 0
 
+    >>> lower_bound([0, 1, 2, 2, 3, 4, 4, 6], 0)
+    0
+    >>> lower_bound([0, 1, 2, 2, 3, 4, 4, 6], 1)
+    0
+    >>> lower_bound([0, 1, 2, 2, 3, 4, 4, 6], 2)
+    2
+    >>> lower_bound([0, 1, 2, 2, 3, 4, 4, 6], 3)
+    4
+    >>> lower_bound([0, 1, 2, 2, 3, 4, 4, 6], 4)
+    5
+    >>> lower_bound([0, 1, 2, 2, 3, 4, 4, 6], 5)
+    7
+    >>> lower_bound([0, 1, 2, 2, 3, 4, 4, 6], 6)
+    8
+    >>> lower_bound([0, 1, 2, 2, 3, 4, 4, 6], 7)
+    8
+    """
+    def search(arr, v, first, last):
+        if v > arr[last]:
+            return len(arr)
+
+        if last >= first:
+            mid = (last + first) // 2
+            if mid == 0:
+                return mid
+
+            if arr[mid] >= v:
+                if arr[mid - 1] < v:
+                    return mid
+                else:
+                    return search(arr, v, first, mid) 
+            else:
+                return search(arr, v, mid + 1, last)
+
+    first = 0
+    last = len(x) - 1
+    return search(x, v, first, last)
 
 def upper_bound(x: list[int], v: int) -> int:
     """Get the index of the upper bound of v in x.
 
     If all values in x are smaller than v, return len(x).
+
+    >>> upper_bound([0, 1, 2, 2, 3, 4, 4, 6], 0)
+    0
+    >>> upper_bound([0, 1, 2, 2, 3, 4, 4, 6], 1)
+    1
+    >>> upper_bound([0, 1, 2, 2, 3, 4, 4, 6], 2)
+    3
+    >>> upper_bound([0, 1, 2, 2, 3, 4, 4, 6], 3)
+    4
+    >>> upper_bound([0, 1, 2, 2, 3, 4, 4, 6], 4)
+    6
+    >>> upper_bound([0, 1, 2, 2, 3, 4, 4, 6], 5)
+    7
+    >>> upper_bound([0, 1, 2, 2, 3, 4, 4, 6], 6)
+    7
+    >>> upper_bound([0, 1, 2, 2, 3, 4, 4, 6], 7)
+    8
     """
-    return 0  # FIXME: Obviously the answer isn't always 0
+    def search(arr, v, first, last):
+        if v > arr[last]:
+            return len(arr)
+
+        if last >= first:
+            mid = (last + first) // 2
+
+            if mid == last:
+                return mid
+
+            if arr[mid] > v:
+                if arr[mid - 1] <= v:
+                    return mid
+                else:
+                    return search(arr, v, first, mid - 1)
+            else:
+                return search(arr, v, mid + 1, last)
+
+    first = 0
+    last = len(x) - 1
+    return search(x, v, first, last)
+    
